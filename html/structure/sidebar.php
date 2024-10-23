2a8d405228759d4b2019a515f3c6529d80e7fc28
kevin ochoa <kevinochoa@example.com>
2024-10-22 20:39:21 -0600

    actializacion hasta el dia 22/10/24 20:40

--- html/structure/sidebar.php
+++ html/structure/sidebar.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/r&d/html/index.php">
-        <div class="sidebar-brand-icon" style="margin-left: -20px;"> 
+        <div class="sidebar-brand-icon" style="margin-left: -20px;">
             <img src="/r&d/img/logo_amcor1.png" alt="Amcor Logo" class="amcor-logo" width="190" height="auto">
         </div>
     </a>
@@ -13,68 +13,58 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">Interface</div>
-
-    <!-- Formulario 1 -->
+    <!-- Menú Forms -->
     <?php if ($role == 1 || $role == 2 || $role == 3): ?>
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm1" aria-expanded="true" aria-controls="collapseForm1">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Formulario 1</span>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForms" aria-expanded="true"
+            aria-controls="collapseForms">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Mis Forms</span>
         </a>
-        <div id="collapseForm1" class="collapse" aria-labelledby="headingForm1" data-parent="#accordionSidebar">
+        <div id="collapseForms" class="collapse" aria-labelledby="headingForms" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Opciones de Formularios:</h6>
-                <a class="collapse-item" href="/r&d/html/forms/form1_create_edit.php">Crear Formulario</a>
-                <a class="collapse-item" href="/r&d/html/forms/form1_list.php">Ver mis forms 1</a>
-            </div>
-        </div>
-    </li>
-    <?php endif; ?>
+                <h6 class="collapse-header">Tipos de Formularios:</h6>
 
-    <!-- Formulario 2 -->
-    <?php if ($role == 1 || $role == 2 || $role == 3): ?>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm2" aria-expanded="true" aria-controls="collapseForm2">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Formulario 2</span>
-        </a>
-        <div id="collapseForm2" class="collapse" aria-labelledby="headingForm2" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Opciones de Formularios:</h6>
-                <a class="collapse-item" href="/r&d/html/forms/form1_list.php">Ver mis forms 2</a>
-            </div>
-        </div>
-    </li>
-    <?php endif; ?>
+                <!-- Form Cotización -->
+                <a class="collapse-item collapsed" href="#" data-toggle="collapse" data-target="#submenuForm1"
+                    aria-expanded="false" aria-controls="submenuForm1">
+                    Form Cotización
+                </a>
+                <div id="submenuForm1" class="collapse bg-lightblue rounded">
+                    <a class="collapse-item text-primary" href="/r&d/html/forms/form1_create_edit.php">Crear
+                        Formulario</a>
+                    <a class="collapse-item text-primary" href="/r&d/html/forms/form1_list.php">Ver mis forms 1</a>
+                </div>
 
-    <!-- Formulario 3 -->
-    <?php if ($role == 1 || $role == 2 || $role == 3): ?>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm3" aria-expanded="true" aria-controls="collapseForm3">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Formulario 3</span>
-        </a>
-        <div id="collapseForm3" class="collapse" aria-labelledby="headingForm3" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Opciones de Formularios:</h6>
-                <a class="collapse-item" href="/r&d/html/form1_create_edit.php">Crear Formulario 3</a>
-                <a class="collapse-item" href="/r&d/html/forms/form1_list.php">Ver mis forms 3</a>
-            </div>
-        </div>
-    </li>
-    <?php endif; ?>
+                <!-- Form Diseño Estructura -->
+                <a class="collapse-item collapsed" href="#" data-toggle="collapse" data-target="#submenuForm2"
+                    aria-expanded="false" aria-controls="submenuForm2">
+                    Form Diseño Estructura
+                </a>
+                <div id="submenuForm2" class="collapse bg-lightblue rounded">
+                    <a class="collapse-item text-primary" href="/r&d/html/forms/form2_list.php">Ver mis forms 2</a>
+                </div>
 
-    <!-- Formulario 4 -->
-    <?php if ($role == 1 || $role == 2 || $role == 3): ?>
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm4" aria-expanded="true" aria-controls="collapseForm4">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Formulario 4</span>
-        </a>
-        <div id="collapseForm4" class="collapse" aria-labelledby="headingForm4" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Opciones de Formularios:</h6>
-                <a class="collapse-item" href="/r&d/html/forms/form1_list.php">Ver mis forms 4</a>
+                <!-- Solicitud Muestra -->
+                <a class="collapse-item collapsed" href="#" data-toggle="collapse" data-target="#submenuForm3"
+                    aria-expanded="false" aria-controls="submenuForm3">
+                    Solicitud Muestra
+                </a>
+                <div id="submenuForm3" class="collapse bg-lightblue rounded">
+                    <a class="collapse-item text-primary" href="/r&d/html/forms/form3_create_edit.php">Crear
+                        Formulario</a>
+                    <a class="collapse-item text-primary" href="/r&d/html/forms/form3_list.php">Ver mis forms 3</a>
+                </div>
+
+                <!-- Form Artículo Muestra -->
+                <a class="collapse-item collapsed" href="#" data-toggle="collapse" data-target="#submenuForm4"
+                    aria-expanded="false" aria-controls="submenuForm4">
+                    Form Artículo Muestra
+                </a>
+                <div id="submenuForm4" class="collapse bg-lightblue rounded">
+                    <a class="collapse-item text-primary" href="/r&d/html/forms/form4_list.php">Ver mis forms 4</a>
+                </div>
+
             </div>
         </div>
     </li>
@@ -83,15 +73,16 @@
     <!-- Ver Todos los Forms (Solo rol 1 y 2) -->
     <?php if ($role == 1 || $role == 2): ?>
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseAllForms" aria-expanded="true" aria-controls="collapseAllForms">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseAllForms"
+            aria-expanded="true" aria-controls="collapseAllForms">
             <i class="fas fa-fw fa-user"></i>
-            <span>Usuarios</span>
+            <span>Approvers</span>
         </a>
         <div id="collapseAllForms" class="collapse" aria-labelledby="headingAllForms" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Validacion de Forms:</h6>
-                <a class="collapse-item" href="/r&d/html/usuarios/forms1_show_all.php">Ver Forms 1</a>
-                <a class="collapse-item" href="/r&d/html/usuarios/forms2_show_all.php">Ver Forms 2</a>
+                <a class="collapse-item" href="/r&d/html/forms/form1_list_all.php">Ver Forms 1</a>
+                <a class="collapse-item" href="/r&d/html/forms/form2_list_all.php">Ver Forms 2</a>
             </div>
         </div>
     </li>
@@ -100,7 +91,8 @@
     <!-- Opciones de Usuarios (Solo rol 1) -->
     <?php if ($role == 1): ?>
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUsers" aria-expanded="true" aria-controls="collapseUsers">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUsers" aria-expanded="true"
+            aria-controls="collapseUsers">
             <i class="fas fa-fw fa-user"></i>
             <span>Usuarios</span>
         </a>
